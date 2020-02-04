94f5fd57488e55a5037b7c75e0138bbded139985
SF2005 <1208733578@qq.com>
2020-02-05 06:19:58 +0800

    jenkins_THExplorer:build:optimize script

--- build.bash
+++ build.bash
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd ${WORK_DIR}
+rm -rf out/${TARGET_CPU}-${BUILDTYPE}/apks
 autoninja -C out/${TARGET_CPU}-${BUILDTYPE} ${TARGET}
+if [ -f out/${TARGET_CPU}-${BUILDTYPE}/apks/*.apk ];then
+    echo Found Artifacts
+    exit 0
+else
+    exit 1
+fi
+
