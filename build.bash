0c4e05883c4a183fbb556f0fee89d5e1bb1c79b9
SF2005 <1208733578@qq.com>
2020-02-15 15:14:37 +0800

    jenkins_THExplorer:add jobs

--- build.bash
+++ build.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ${WORK_DIR}
 rm -rf out/${TARGET_CPU}-${BUILDTYPE}/apks
-autoninja -C out/${TARGET_CPU}-${BUILDTYPE} ${TARGET}
+autoninja -j${JOBS} -C out/${TARGET_CPU}-${BUILDTYPE} ${TARGET}
 if [ -f out/${TARGET_CPU}-${BUILDTYPE}/apks/*.apk ];then
     echo Found Artifacts
     exit 0
