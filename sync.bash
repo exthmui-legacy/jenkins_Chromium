ee0fc80b79d6d33a6b44de556f6fd7e960e981d2
ISNing <33572057+ISNing@users.noreply.github.com>
2020-02-16 15:02:16 +0800

    Update sync.bash

--- sync.bash
+++ sync.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd ${WORK_DIR}
-git pull
+git pull origin master：master --tags -f
+git reset --hard ${VERSION_TAG}
+gclient sync --force --nohooks --with_branch_heads
+gclient runhooks
+# You can get latest chromium release tags here:https://omahaproxy.appspot.com/
